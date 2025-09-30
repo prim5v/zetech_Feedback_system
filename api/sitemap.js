6bddefa79d9b0f0ab942dc59753758e7912ce9d9
Prim5v <emax27348@gmail.com>
2025-09-30 23:29:27 +0300

    Add dynamic sitemap generator from src/pages

--- api/sitemap.js
+++ api/sitemap.js
@@ -13,7 +13,8 @@ export default function handler(req, res) {
     { loc: `${baseUrl}/student/issue-details`, priority: "0.8" },
   ];
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  const xml = `<?xml version="1.0" encoding="UTF-8"?> 
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls
   .map(
     (u) => `  <url>
